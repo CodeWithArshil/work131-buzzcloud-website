05663fdce1efa6b931af94813042a4ce0f1403e9
mumithawala <mohammedmithawala8@gmail.com>
2023-03-11 16:06:31 +0530

    done

---
+++ project/elements/footer.php
@@ -0,0 +1,87 @@
+<footer>
+        <div class="footer-area">
+            <div class="footer-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="footer-widget">
+                                <div class="logo">
+                                    <a href="index-2.html"><img src="assets/img/logo/Buzzcloud-Logo.png" alt=""></a>
+                                </div>
+                                <div class="footer-social">
+                                    <ul class="list-wrap">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li class="active"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="footer-contact">
+                                    <span>For Support</span>
+                                    <h2 class="title"><a href="tel:0123456789">7984104006</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="footer-widget">
+                                <h4 class="fw-title">Primary Pages</h4>
+                                <div class="fw-link">
+                                    <ul class="list-wrap">
+                                        <li><a href="index-2.html">Home</a></li>
+                                        <li><a href="about-us.html">About</a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                        <li><a href="services-details.html">Service</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="footer-widget">
+                                <h4 class="fw-title">Pages</h4>
+                                <div class="fw-link">
+                                    <ul class="list-wrap">
+                                        <li><a href="about-us.html">About</a></li>
+                                        <li><a href="contact.html">Pricing</a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                        <li><a href="contact.html">Request for Demo</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="footer-widget">
+                                <h4 class="fw-title">About Store</h4>
+                                <div class="footer-about">
+                                    <ul class="list-wrap">
+                                        <li><img src="assets/img/icon/phone_icon.svg" alt=""><a
+                                                href="tel:0123456789">7984104006</a></li>
+                                        <li><img src="assets/img/icon/mail_icon.svg" alt=""><a
+                                                href="mailto:mediabuzzcloud@gmail.com">mediabuzzcloud@gmail.com</a></li>
+                                        <li><img src="assets/img/icon/loction_icon.svg" alt=""><span>21st floor, D
+                                                Block, Westgate by true value, Sarkhej - Gandhinagar Hwy, Makarba,
+                                                Ahmedabad, Gujarat 380015</span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-6">
+                            <div class="cart-img">
+                                <img src="assets/img/images/cart_img.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="copyright-text text-end">
+                                <p>© 2023 xilio. All Rights Reserved</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
